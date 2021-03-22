b41c2dd7453218542088d4286af002a10a7bfe40
B.ROCHAN SINGH <bahadur.rochan@gmail.com>
2021-03-22 22:24:09 +0530

    dealt with cors

--- chat-server/index.js
+++ chat-server/index.js
@@ -1,18 +1,21 @@
-var express = require("express");
-var app = express();
-var socket = require("socket.io");
+const express = require("express");
+const app = express();
+const httpServer = require("http").createServer(app);
+const options = {
+  cors: true,
+  origins: ["http://localhost:3000"],
+};
+const io = require("socket.io")(httpServer, options);
 
 const port = 5000;
 
-var server = app.listen(port, function () {
+httpServer.listen(port, function () {
   console.log("Listening on port " + port);
 });
 
 app.use(express.static("public"));
 
-var io = socket(server);
-
-io.on("connection", function (socket) {
+io.on("connection", (socket) => {
   console.log("Socket connection made " + socket.id);
   socket.on("chat", function (data) {
     io.sockets.emit("chat", data);
